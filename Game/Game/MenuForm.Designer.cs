3beaef146f05f271e8e21ceb3c3934afda509c51
dimitarmileski <dimit@r.com>
2019-06-26 22:42:50 +0200

    Manu Form Design

--- Game/Game/MenuForm.Designer.cs
+++ Game/Game/MenuForm.Designer.cs
@@ -28,80 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuForm));
             this.btnStart = new System.Windows.Forms.Button();
             this.btnOptions = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnAbout = new System.Windows.Forms.Button();
+            this.menuDogPicture = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.menuDogPicture)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(387, 79);
+            this.btnStart.BackColor = System.Drawing.Color.DarkGray;
+            this.btnStart.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnStart.Location = new System.Drawing.Point(0, 248);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(175, 96);
+            this.btnStart.Size = new System.Drawing.Size(319, 96);
             this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Text = "> Start";
+            this.btnStart.UseVisualStyleBackColor = false;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // btnOptions
             // 
-            this.btnOptions.Location = new System.Drawing.Point(387, 202);
+            this.btnOptions.BackColor = System.Drawing.Color.DarkGray;
+            this.btnOptions.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOptions.ForeColor = System.Drawing.Color.White;
+            this.btnOptions.Location = new System.Drawing.Point(0, 367);
             this.btnOptions.Name = "btnOptions";
-            this.btnOptions.Size = new System.Drawing.Size(175, 53);
+            this.btnOptions.Size = new System.Drawing.Size(319, 96);
             this.btnOptions.TabIndex = 1;
-            this.btnOptions.Text = "Options";
-            this.btnOptions.UseVisualStyleBackColor = true;
+            this.btnOptions.Text = "⦾ Options";
+            this.btnOptions.UseVisualStyleBackColor = false;
             this.btnOptions.Click += new System.EventHandler(this.btnOptions_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(387, 365);
+            this.btnExit.BackColor = System.Drawing.Color.DarkGray;
+            this.btnExit.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(0, 629);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(175, 53);
+            this.btnExit.Size = new System.Drawing.Size(319, 96);
             this.btnExit.TabIndex = 2;
-            this.btnExit.Text = "Exit";
-            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Text = "☢ Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnAbout
             // 
-            this.btnAbout.Location = new System.Drawing.Point(387, 286);
+            this.btnAbout.BackColor = System.Drawing.Color.DarkGray;
+            this.btnAbout.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbout.ForeColor = System.Drawing.Color.White;
+            this.btnAbout.Location = new System.Drawing.Point(0, 497);
             this.btnAbout.Name = "btnAbout";
-            this.btnAbout.Size = new System.Drawing.Size(175, 53);
+            this.btnAbout.Size = new System.Drawing.Size(319, 96);
             this.btnAbout.TabIndex = 3;
-            this.btnAbout.Text = "About";
-            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Text = "⦾  About";
+            this.btnAbout.UseVisualStyleBackColor = false;
             this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // menuDogPicture
+            // 
+            this.menuDogPicture.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.menuDogPicture.Image = ((System.Drawing.Image)(resources.GetObject("menuDogPicture.Image")));
+            this.menuDogPicture.Location = new System.Drawing.Point(0, 76);
+            this.menuDogPicture.Name = "menuDogPicture";
+            this.menuDogPicture.Size = new System.Drawing.Size(1276, 673);
+            this.menuDogPicture.TabIndex = 5;
+            this.menuDogPicture.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Consolas", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(485, 176);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "MOPS.";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = global::Game.Properties.Resources.background;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(732, 248);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(697, 528);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.Size = new System.Drawing.Size(157, 150);
+            this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
             // MenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 528);
+            this.ClientSize = new System.Drawing.Size(1276, 749);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAbout);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnOptions);
             this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuDogPicture);
             this.Name = "MenuForm";
             this.Text = "Menu";
             this.Load += new System.EventHandler(this.MenuForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.menuDogPicture)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,6 +151,8 @@
         private System.Windows.Forms.Button btnOptions;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.PictureBox menuDogPicture;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
